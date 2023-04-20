5f0628b2f4a71bc8128b7935d61f50000f80263f
Defaultniy <106524795+Defaultniy@users.noreply.github.com>
2023-04-20 20:14:51 +0300

    Delete CursesDB directory

--- CursesDB/src/main/java/WithEM/Methods/Service/Students_service.java
+++
@@ -1,9 +0,0 @@
-package WithEM.Methods.Service;
-
-import java.util.List;
-
-public interface Students_service {
-    //Средняя оценка студетов по курсам
-    List<Double> AvgMarkOnCurs();
-
-}
--- CursesDB/target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-insert into GROUP_OF_CURSES(ID, GROUP_CURSES, YEAR_OF_ENTER) values (1, 1, 2022);
-insert into STUDENTS (ID, FIO, ID_GROUP, CURSES_NAME, MARK) values (1, 'Ivanov Ivan Jovanovich', 1, 'DB', 5);
